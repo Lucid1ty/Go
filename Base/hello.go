29638dfb009f3d06fd6b48ed1d49602b0897dd29
Lucidity <2993514952@qq.com>
2022-05-18 00:47:24 +0800

    add some code

--- Base/hello.go
+++ Base/hello.go
@@ -1,8 +1,14 @@
-package main	//把这个 test.go 归属到main
-import "fmt"	//引入一个包 fmt
+package main //把这个 test.go 归属到main
+import "fmt" //引入一个包 fmt
+
 func main() {
 	//输出Hello World!
 	fmt.Println("Hello World!")
-	
-
+	fmt.Println("Hello World!")
+	fmt.Println("Hello World!")
+	fmt.Println("Hello World!")
+	fmt.Println("Hello World!")
+	fmt.Println("Hello World!")
+	fmt.Println("Hello World!")
+	fmt.Println("Hello World!")
 }
